e0ba609e0a11a357cbc8df92ac186e4d8d373d0f
Jongwon <lastone9182@gmail.com>
2017-01-04 05:11:54 +0900

    indexing

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,15 @@
 // new tab from 'a' link
-$('.post-content a').attr('target','_blank')
+$('.post-content>a').attr('target','_blank');
+
+// make bookmark
+$('.post-content>h2').wrap('<a href="#index-table" style="text-decoration:none" ></a>');
+
+// append index
+$('.post-content>a>h2').each(function(){
+    var text = $(this).text();
+    var link = text.replace(/[\.,'’]|[0-9]/g, '');
+    link = link.trim(link);
+    link = link.replace(/ /g, '-');
+    link = link.toLowerCase();
+    return $("<a href='#" + link + "'>" + text + "</a><br>").appendTo('#index-table');
+});
